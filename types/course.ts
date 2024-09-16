f5f530500b6ebc577870606669a5879acfb751d7
jhchoi <choi.jihyun154@gmail.com>
2024-09-16 23:49:37 +0900

    commit others

---
+++ types/course.ts
@@ -0,0 +1,23 @@
+export interface Course {
+  title: string;
+  subtitle: string;
+  courseSlug: string;
+  content: string;
+  thumbnail: string;
+  video: string;
+  rating: number;
+  reviewsCount: number;
+  studentCount: number;
+  reviewsUrl: string;
+  inflearnUrl: string;
+  gymcodingUrl: string;
+}
+
+type NewType = Omit<Course, 'rating' | 'reviewsCount' | 'studentCount'>;
+
+export interface CourseWithPath extends NewType {
+  rating: string;
+  reviewsCount: string;
+  studentCount: string;
+  path: string;
+}
